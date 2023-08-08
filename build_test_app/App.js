9224d567c8d459abe60046e0fdb7a68f87ba9157
sungjuncho <chotjdwns654@gmail.com>
2023-08-08 10:29:56 +0900

    fcm / dependencies test

---
+++ build_test_app/App.js
@@ -0,0 +1,73 @@
+import { StatusBar } from 'expo-status-bar';
+import React ,  { useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+
+export default function App() {
+
+  const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()){
+      messaging().getToken().then(token => {
+        console.log(token);
+      })
+    }
+    else{
+      console.log("Failed token status" , authStatus);
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then( async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      });
+
+
+      messaging().onNotificationOpenedApp((remoteMessage) => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+      });
+
+      messaging().setBackgroundMessageHandler(async remoteMessage => {
+        console.log('Message handled in the background!', remoteMessage);
+      });
+
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      });
+
+      return unsubscribe;
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>FCM tutorial</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
